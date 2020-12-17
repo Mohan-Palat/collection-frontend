81909213e30b36fcf5150e9cffd55c2b1c0f3bba
Aaron Lauson <alauson@gmail.com>
2020-12-17 12:26:28 -0600

    rebase frontend

--- backEnd/app/models/accessory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'); 
-  
-var accessorySchema = new mongoose.Schema({  
-    accessoryName: String,
-    doHave: Boolean,
-    imageId: Object
-  });
-  
-module.exports = new mongoose.model('Accessory', accessorySchema); 
